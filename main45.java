95cb26746eac017738215a7b2116cf8f759040ad
whiitees <142717675+whiitees@users.noreply.github.com>
2024-03-08 21:49:04 +0100

    Add files via upload

---
+++ main45.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * factorial(n-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println("Factorial of " + num + " is " + factorial(num));
+    }
+}
